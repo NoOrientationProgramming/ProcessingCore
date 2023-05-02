ba0dd6f48e196a2168084c1f96fddf9134cab423
Johannes Natter <office@dsp-crowd.com>
2023-05-02 21:51:44 +0200

    common: Add module Res

---
+++ common_GPLv3/Res.h
@@ -0,0 +1,34 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 31.03.2018
+
+  Copyright (C) 2018 Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RES_H
+#define RES_H
+
+#include "resources.h"
+
+const Resource *resourceFind(const char *pName);
+
+#endif
+
