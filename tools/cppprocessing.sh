fc8cf098e88a43e1b78992bdc3b7ce86310ba11f
Johannes Natter <office@dsp-crowd.com>
2023-09-21 16:40:53 +0200

    tools: Update cppprocessing.sh

--- tools/cppprocessing.sh
+++ tools/cppprocessing.sh
@@ -40,36 +40,89 @@ theyear=$(date +%Y)
 
 # CPP file
 cat > $cppfilename << EOF
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on ${thedate}
+
+  Copyright (C) ${theyear}-now Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "$hppfilename"
 
+#define dForEach_ProcState(gen) \\
+		gen(StStart) \\
+		gen(StMain) \\
+		gen(StTmp) \\
+
+#define dGenProcStateEnum(s) s,
+dProcessStateEnum(ProcState);
+
+#if 1
+#define dGenProcStateString(s) #s,
+dProcessStateStr(ProcState);
+#endif
+
 using namespace std;
+
+#define LOG_LVL	0
 
 ${classname}::${classname}()
 	: Processing("${classname}")
+	, mState(StStart)
+	, mStartMs(0)
 {}
 
 /* member functions */
 
-Success ${classname}::initialize()
-{
-	return Positive;
-}
-
 Success ${classname}::process()
 {
-	return Pending;
-}
+	//uint32_t curTimeMs = millis();
+	//uint32_t diffMs = curTimeMs - mStartMs;
+	//Success success;
+#if 0
+	procWrnLog("mState = %s", ProcStateString[mState]);
+#endif
+	switch (mState)
+	{
+	case StStart:
 
-Success ${classname}::shutdown()
-{
-	return Positive;
+		break;
+	case StMain:
+
+		break;
+	case StTmp:
+
+		break;
+	default:
+		break;
+	}
+
+	return Pending;
 }
 
 void ${classname}::processInfo(char *pBuf, char *pBufEnd)
 {
-	(void)pBuf;
-	(void)pBufEnd;
+#if 1
+	dInfo("State\t\t\t%s\n", ProcStateString[mState]);
+#endif
 }
 
 /* static functions */
@@ -78,6 +131,30 @@ EOF
 
 # Header file
 cat > $hppfilename << EOF
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on ${thedate}
+
+  Copyright (C) ${theyear}-now Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef ${headerdef}_H
 #define ${headerdef}_H
@@ -110,12 +187,12 @@ private:
 	 */
 
 	/* member functions */
-	Success initialize();
 	Success process();
-	Success shutdown();
 	void processInfo(char *pBuf, char *pBufEnd);
 
 	/* member variables */
+	uint32_t mState;
+	uint32_t mStartMs;
 
 	/* static functions */
 
